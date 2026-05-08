b3b8c05a3c2d790abd9054afcc31e2d3dfe1f7f9
Khaoula <ton.email@exemple.com>
2026-05-08 15:34:56 +0200

    feat: add ArgoCD GitOps configuration

---
+++ argocd/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl wait --for=condition=available --timeout=300s deployment/argocd-server -n argocd
+kubectl apply -f application.yaml
+echo "ArgoCD installé !"
